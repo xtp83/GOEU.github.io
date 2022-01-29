39c69695b8305092c8cbf2b4bcdd179fb3fed2b4
xtp83 <95931055+xtp83@users.noreply.github.com>
2022-01-29 10:57:43 +0100

    Delete logo.js

--- js/logo.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-	$(window).scroll(function() {
-		if ($(window).scrollTop()>=500) {
-			$(".cabecera").removeClass("menu-grande").addClass("menu-pequenio");
-		}else{
-			$(".cabecera").removeClass("menu-pequenio").addClass("menu-grande");
-		}
-	});
-});
